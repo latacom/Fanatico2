8cf3f3717c996747ebebce2a04809aac5159571e
latacom <luisperesito30@gmail.com>
2018-06-13 16:40:30 -0400

    Commit Pruebas Unitarias

---
+++ app/src/androidTest/java/udinova/fanatico/TestLogin.java
@@ -0,0 +1,13 @@
+package udinova.fanatico;
+
+import junit.framework.Assert;
+
+import udinova.fanatico.Models.JSON.UsuarioJSON;
+import  org.junit.Test;
+
+
+public class TestLogin {
+    @Test
+    public void login_isCorrect() throws Exception {
+    }
+}
