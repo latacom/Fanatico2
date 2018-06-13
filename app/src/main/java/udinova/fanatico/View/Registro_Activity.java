0243e2550e8243db8ef005b6e2300dc1eab9f065
latacom <luisperesito30@gmail.com>
2018-06-12 22:12:17 -0400

    Initial commit

---
+++ app/src/main/java/udinova/fanatico/View/Registro_Activity.java
@@ -0,0 +1,18 @@
+package udinova.fanatico.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import udinova.fanatico.R;
+
+
+public class Registro_Activity extends AppCompatActivity {
+
+    private String var;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+
+}
